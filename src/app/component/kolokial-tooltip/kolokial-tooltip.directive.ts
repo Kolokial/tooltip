3cb70e4b45aa8c4edfdd52f01915fba3fa5f5fc2
Kolokial <gary_luck@me.com>
2018-05-05 23:45:34 +0100

    added onMouseLeave method and respective destroy method. Added ngOndestroy and added mouse enter/leave methods to destroy.

--- src/app/component/kolokial-tooltip/kolokial-tooltip.directive.ts
+++ src/app/component/kolokial-tooltip/kolokial-tooltip.directive.ts
@@ -1,23 +1,38 @@
-import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, OnDestroy } from '@angular/core';
 import { KolokialTooltipService } from './kolokial-tooltip.service';
 
 @Directive({
   selector: '[appKolokialTooltip]'
 })
-export class KolokialTooltipDirective {
+export class KolokialTooltipDirective implements OnDestroy {
 
   @Input('appKolokialTooltip') appKolokialTooltip: string;
 
-  private onMouseEnterDestory: () => void;
+  private onMouseEnterDestroy: () => void;
+  private onMouseLeaveDestroy: () => void;
 
   constructor(private renderer: Renderer2, private hostElement: ElementRef, private tooltipService: KolokialTooltipService) {
     this.onMouseEnter();
+    this.onMouseLeave();
+  }
+
+  public ngOnDestroy(): void {
+    if (this.onMouseEnterDestroy && this.onMouseLeaveDestroy) {
+      this.onMouseEnterDestroy();
+      this.onMouseLeaveDestroy();
+    }
   }
 
   private onMouseEnter(): void {
-    this.onMouseEnterDestory = this.renderer.listen(this.hostElement.nativeElement, 'mouseenter', () => {
-      const hostPosition: ClientRect = this.hostElement.nativeElement.getBoundingRect();
+    this.onMouseEnterDestroy = this.renderer.listen(this.hostElement.nativeElement, 'mouseenter', () => {
+      const hostPosition: ClientRect = this.hostElement.nativeElement.getBoundingClientRect();
       this.tooltipService.showTooltip(this.appKolokialTooltip, hostPosition);
+    });
+  }
+
+  private onMouseLeave(): void {
+    this.onMouseLeaveDestroy = this.renderer.listen(this.hostElement.nativeElement, 'mouseleave', () => {
+      this.tooltipService.hideTooltip(this.appKolokialTooltip);
     });
   }
 
