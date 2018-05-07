bed5eb9cd9f66944ab75977474aa1110e37b838b
Kolokial <gary_luck@me.com>
2018-05-07 13:09:41 +0100

    started adding basic top/bottom position.

--- src/app/component/kolokial-tooltip/kolokial-tooltip.component.ts
+++ src/app/component/kolokial-tooltip/kolokial-tooltip.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, TemplateRef, ContentChild, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ContentChild, Input, OnDestroy, ElementRef } from '@angular/core';
 import { KolokialTooltipService } from './kolokial-tooltip.service';
 import { KolokialTooltipAlignment, KolokialTooltipPosition } from './kolokial-tooltip.enum';
+import { StringifyOptions } from 'querystring';
 
 type Alignment = KolokialTooltipAlignment;
 type Position = KolokialTooltipPosition;
@@ -20,14 +21,20 @@ export class KolokialTooltipComponent implements OnInit, OnDestroy {
   private alignments = KolokialTooltipAlignment;
   private positions = KolokialTooltipPosition;
 
+  private visibility: boolean;
   private position: { [key: string]: number } = {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 20,
+    top: null,
+    right: null,
+    bottom: null,
+    left: null,
   };
 
+  private tooltipDimensions: ClientRect;
+  private hostLeft: number;
+  private hostTop: number;
+
   @ContentChild(TemplateRef) private templateVariable: TemplateRef<any>;
+  @ViewChild('tooltipContainer') private tooltipContainer: ElementRef;
 
   @Input() public set templateKey(value: string) {
     this._key = value;
@@ -38,9 +45,14 @@ export class KolokialTooltipComponent implements OnInit, OnDestroy {
     this._text = value;
   }
 
+  public get text(): string {
+    return this._text;
+  }
+
   constructor(private tooltipService: KolokialTooltipService) { }
 
   ngOnInit() {
+    console.log(this.tooltipDimensions);
   }
 
   ngOnDestroy(): void {
@@ -48,27 +60,48 @@ export class KolokialTooltipComponent implements OnInit, OnDestroy {
   }
 
   public show(hostDimension: ClientRect): void {
-    console.log(hostDimension);
+    this.visibility = true;
+    this.setPosition(hostDimension);
   }
 
   public hide(): void {
-
+    this.visibility = false;
   }
 
   private setPosition(hostPosition: ClientRect): void {
-    switch (this._displayPosition) {
-      case this.positions.Top: this.setPositionTop(hostPosition); break;
-      case this.positions.Right: this.setPositionRight(hostPosition); break;
-      case this.positions.Bottom: this.setPositionBottom(hostPosition); break;
-      case this.positions.Left: this.setPositionLeft(hostPosition); break;
+    this.setTooltipDimensions();
+    if (hostPosition.left !== this.hostLeft || hostPosition.top !== this.hostTop) {
+      switch (this._displayPosition) {
+        case this.positions.Top: this.setPositionTop(hostPosition); break;
+        case this.positions.Right: this.setPositionRight(hostPosition); break;
+        case this.positions.Bottom: this.setPositionBottom(hostPosition); break;
+        case this.positions.Left: this.setPositionLeft(hostPosition); break;
+      }
+
+      this.hostLeft = hostPosition.left;
+      this.hostTop = hostPosition.top;
     }
   }
 
   private setPositionTop(hostPosition: ClientRect): void {
+    this.position.bottom = hostPosition.top + 15;
+    this.position.top = hostPosition.top - 15 - this.tooltipDimensions.height;
+
+    if (this.position.top < 0) {
+      this.setPositionBottom(hostPosition);
+      return;
+    }
 
   }
 
   private setPositionBottom(hostPosition: ClientRect): void {
+    this.position.top = hostPosition.bottom + 15;
+    this.position.bottom = hostPosition.bottom + 15 + this.tooltipDimensions.height;
+
+    if (this.position.bottom < 0) {
+      this.setPositionTop(hostPosition);
+      return;
+    }
 
   }
 
@@ -78,6 +111,13 @@ export class KolokialTooltipComponent implements OnInit, OnDestroy {
 
   private setPositionLeft(hostPosition: ClientRect): void {
 
+  }
+
+  private setTooltipDimensions() {
+    if (this.tooltipDimensions) {
+      return this.tooltipDimensions;
+    }
+    return this.tooltipDimensions = this.tooltipContainer.nativeElement.getBoundingClientRect();
   }
 
   private getUnusedAlignments() {
